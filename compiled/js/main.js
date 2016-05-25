d9ee5fd28ff539e9e356e4f2a8265cb73948241c
Michael Reynolds <michael4reynolds@gmail.com>
2016-05-25 16:54:42 -0400

    refactor display most recent

--- compiled/js/main.js
+++ compiled/js/main.js
@@ -1,60 +1,62 @@
 'use strict';
 
-function getRequest(searchTerm) {
-  var _pageToken = arguments.length <= 1 || arguments[1] === undefined ? '' : arguments[1];
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-  // prepare the request
+function getRequest(searchTerm) {
+  var max = arguments.length <= 1 || arguments[1] === undefined ? 3 : arguments[1];
+  var orderby = arguments.length <= 2 || arguments[2] === undefined ? 'date' : arguments[2];
+
+  var _pageToken = arguments.length <= 3 || arguments[3] === undefined ? '' : arguments[3];
+
   var params = {
     part: 'snippet',
     key: 'AIzaSyB9evlzwtgDeMvhbSA-zVYUBQf_K1jcWfc',
     q: searchTerm + ' recipe',
     type: 'video',
-    maxResults: 3,
-    order: 'date',
+    maxResults: max,
+    order: orderby,
     regionCode: 'US',
     fields: 'nextPageToken,prevPageToken,' + 'items(id(videoId)),' + 'items(snippet(title,thumbnails(high)))',
     pageToken: _pageToken
   };
   var url = "https://www.googleapis.com/youtube/v3/search";
-
-  // execute the request
+  return [url, params];
+}
+function displayMostRecent(url, params) {
   $.get(url, params, function (data) {
-    displayMostRecents(data);
+    var $recents = $('.recents.videos');
+    $recents.html('');
+
+    var index = 1;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = data.items[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var value = _step.value;
+
+        var thumb = '\n      <a href="https://www.youtube.com/watch?v=' + value.id.videoId + '" target="_blank">\n        <img src="' + value.snippet.thumbnails.high.url + '" alt="recent video ' + index++ + '">\n      </a>\n    ';
+        $recents.append(thumb);
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
   }, 'json');
 }
 
-var displayMostRecents = function displayMostRecents(data) {
-  $('.recents.videos').html('');
-
-  var index = 1;
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
-
-  try {
-    for (var _iterator = data.items[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var value = _step.value;
-
-      var vidframe = '\n      <a href="https://www.youtube.com/watch?v=' + value.id.videoId + '" target="_blank">\n        <img src="' + value.snippet.thumbnails.high.url + '" alt="recent video ' + index++ + '">\n      </a>\n    ';
-      $('.recents.videos').append(vidframe);
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
-    }
-  }
-};
-
 $(function () {
-  getRequest('');
+  displayMostRecent.apply(undefined, _toConsumableArray(getRequest('')));
 });
 //# sourceMappingURL=main.js.map
--- js/main.js
+++ js/main.js
@@ -1,41 +1,38 @@
-function getRequest(searchTerm, _pageToken = '') {
-  // prepare the request
+function getRequest(searchTerm, max=3, orderby='date', _pageToken='') {
   let params = {
     part: 'snippet',
     key: 'AIzaSyB9evlzwtgDeMvhbSA-zVYUBQf_K1jcWfc',
     q: searchTerm + ' recipe',
     type: 'video',
-    maxResults: 3,
-    order: 'date',
+    maxResults: max,
+    order: orderby,
     regionCode: 'US',
     fields: 'nextPageToken,prevPageToken,' +
-    'items(id(videoId)),' +
-    'items(snippet(title,thumbnails(high)))',
+            'items(id(videoId)),' +
+            'items(snippet(title,thumbnails(high)))',
     pageToken: _pageToken
   }
   let url = "https://www.googleapis.com/youtube/v3/search"
-
-  // execute the request
-  $.get(url, params, function (data) {
-    displayMostRecents(data)
-  }, 'json')
+  return [url, params]
 }
+function displayMostRecent(url, params) {
+  $.get(url, params, function (data) {
+    var $recents = $('.recents.videos')
+    $recents.html('')
 
-let displayMostRecents = function (data) {
-  $('.recents.videos').html('')
-
-  let index = 1
-  for (let value of data.items) {
-    let vidframe = `
+    let index = 1
+    for (let value of data.items) {
+      let thumb = `
       <a href="https://www.youtube.com/watch?v=${value.id.videoId}" target="_blank">
         <img src="${value.snippet.thumbnails.high.url}" alt="recent video ${index++}">
       </a>
     `
-    $('.recents.videos').append(vidframe)
-  }
+      $recents.append(thumb)
+    }
+  }, 'json')
 }
 
 $(function () {
-  getRequest('')
+  displayMostRecent(...getRequest(''))
 })
 
